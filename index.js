f21cd9d76fb23ffccc366665881b7d51e046b1fe
Fabien Steines <fabien.steines@gmail.com>
2016-08-31 10:54:12 +0200

    included logger

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 "use strict";
 var express = require('express');
+var winston = require('winston');
+var myFunc = require("./myfunction.js");
 var app = express();
-var myFunc = require("./myfunction.js")
 
-app.get('/hello',function(req,res) {
 
-	try {
-	    var x = 2;// / 0;
-	    throw "holy cow ! " + myFunc.cube(3);
-
-	    res.json({message:"Hello World",val:x});
-	} catch(err) {
-	    res.json({error:err});
-	}
+/**
+ * This gets always called once a client uses the api
+ */
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
-var server = app.listen(8081, function () {
 
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
+/**
+ * This is the REST hello method
+ * In case of any error, this will return the json error string
+ */
+app.get('/hello', function (req, res) {
+
+    try {
+
+        var x = Math.floor(Math.random() * 10);
+        if (x == 2 || x == 8) throw "holy cow ! " + myFunc.cube(3);
+        res.json({message: "The number is " + x});
+
+    } catch (err) {
+        winston.log("error", "There has been the holy cow error");
+        res.json({error: err});
+    }
+
+});
+
+/**
+ * We start our WebService here
+ * @type {http.Server}
+ */
+var server = app.listen(8000, function () {
+
+    winston.add(winston.transports.File, {filename: 'logs/myNiceLog.json'});
+    var host = server.address().address;
+    var port = server.address().port;
+    winston.log("info","Sample Javascript web service http://%s:%s", host, port);
 
 });
